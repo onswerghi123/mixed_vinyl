b4ad3f152bfe97a5949ba895feaef02177fd4e22
ons werghi <onsw5500@gmail.com>
2023-05-16 00:47:07 +0100

    The Query Builder

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -1,50 +1,66 @@
 <?php
 
-namespace App\Controller;
+namespace App\Repository;
 
-use App\Repository\VinylMixRepository;
-use App\Service\MixRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
+use App\Entity\VinylMix;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class VinylController extends AbstractController
+/**
+ * @extends ServiceEntityRepository<VinylMix>
+ *
+ * @method VinylMix|null find($id, $lockMode = null, $lockVersion = null)
+ * @method VinylMix|null findOneBy(array $criteria, array $orderBy = null)
+ * @method VinylMix[]    findAll()
+ * @method VinylMix[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class VinylMixRepository extends ServiceEntityRepository
 {
-    public function __construct(
-        private bool $isDebug,
-        private MixRepository $mixRepository
-    )
-    {}
-
-    #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    public function __construct(ManagerRegistry $registry)
     {
-        $tracks = [
-            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
-            ['song' => 'Waterfalls', 'artist' => 'TLC'],
-            ['song' => 'Creep', 'artist' => 'Radiohead'],
-            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
-        ];
-
-        return $this->render('vinyl/homepage.html.twig', [
-            'title' => 'PB & Jams',
-            'tracks' => $tracks,
-        ]);
+        parent::__construct($registry, VinylMix::class);
     }
 
-    #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
+    public function add(VinylMix $entity, bool $flush = false): void
     {
-        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $this->getEntityManager()->persist($entity);
 
-        $mixes = $mixRepository->findAll();
-
-        return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre,
-            'mixes' => $mixes,
-        ]);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
     }
+
+    public function remove(VinylMix $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * @return VinylMix[] Returns an array of VinylMix objects
+     */
+    public function findByExampleField($value): array
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('v.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+//    public function findOneBySomeField($value): ?VinylMix
+//    {
+//        return $this->createQueryBuilder('v')
+//            ->andWhere('v.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
